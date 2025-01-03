a30d322ec82a1b1b03020a8695896ae571b7872b
ibell13 <isaac@hobu.co>
2025-01-03 12:03:14 -0600

    initial test skeleton

---
+++ test/unit/ArbiterUtilsTest.cpp
@@ -0,0 +1,32 @@
+
+#include <pdal/pdal_test_main.hpp>
+
+#include <pdal/PDALUtils.hpp>
+
+#include "Support.hpp"
+
+namespace pdal
+{
+
+TEST(ArbiterUtilsTest, tempFilename)
+{}
+
+TEST(ArbiterUtilsTest, fileSize)
+{}
+
+TEST(ArbiterUtilsTest, createFile)
+{}
+
+TEST(ArbiterUtilsTest, fetchRemote)
+{}
+
+TEST(ArbiterUtilsTest, openFile)
+{}
+
+TEST(ArbiterUtilsTest, fileExists)
+{}
+
+TEST(ArbiterUtilsTest, globTest)
+{}
+
+}//namespace pdal
