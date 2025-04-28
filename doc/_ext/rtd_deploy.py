c7517f1f71458ce599dca0a50e4a3f8e154641d4
ibell13 <isaac@hobu.co>
2025-04-28 15:35:00 -0500

    Update rtd_deploy.py

--- doc/_ext/rtd_deploy.py
+++ doc/_ext/rtd_deploy.py
@@ -2,4 +2,5 @@ import os
 
 def setup(app):
     if os.environ.get('READTHEDOCS') == 'True':
+        print("\n\n\n\n\n\nrtd_deploy\n\n\n\n\n\n")
         app.config["html_baseurl"] = os.environ['READTHEDOCS_CANONICAL_URL']
