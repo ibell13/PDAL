c24a237439afb51e16db467dc1595ad580a465f5
ibell13 <isaac@hobu.co>
2025-04-28 14:12:47 -0500

    Update rtd-prebuild.sh

--- doc/rtd-prebuild.sh
+++ doc/rtd-prebuild.sh
@@ -14,4 +14,4 @@ cmake \
 # Target makes dimension-table.csv, runs doxygen
 cmake --build . --target doxygen
 cmake --build . --target jupyterbook_config
-sed -i "1i\import os, sys; sys.path.append(os.path.abspath(\"./_ext\"))" conf.py
+sed -i "1i\import os, sys; sys.path.append(os.path.abspath(\"./_ext\"))" ../conf.py
